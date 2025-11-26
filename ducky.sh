1ea2dcfc46dfee12a8077a6457361be35a266f6d
abotias <abotias3@gmail.com>
2025-11-26 23:54:13 +0100

    Update image path in ducky.sh to use img.png and modify wget command

--- ducky.sh
+++ ducky.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #!/usr/bin/env bash
 
-IMG="$HOME/Pictures/ducky.png"
+IMG="$HOME/Pictures/img.png"
 
-wget -q -O "$IMG" "https://raw.githubusercontent.com/abotias/scripts/refs/heads/main/img/ducky.png"
+wget -q -O "$IMG" "https://cataas.com/cat"
+if [ $? -ne 0 ]; then wget -q -O "$IMG" "https://raw.githubusercontent.com/abotias/scripts/refs/heads/main/img/ducky.png"; fi
 
 gsettings set org.gnome.desktop.background picture-uri "file://$IMG"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMG"
