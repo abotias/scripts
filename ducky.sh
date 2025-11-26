6f7efe2cc50741501c9e36d561d38c4b80821bfb
abotias <abotias3@gmail.com>
2025-11-26 23:03:48 +0100

    Add missing newline at end of file in ducky.sh

--- ducky.sh
+++ ducky.sh
@@ -7,3 +7,4 @@ wget -q -O "$IMG" "https://raw.githubusercontent.com/abotias/scripts/refs/heads/
 
 gsettings set org.gnome.desktop.background picture-uri "file://$IMG"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMG"
+exit 0
