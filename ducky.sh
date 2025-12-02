5dd3e5578df48ddcbe1be8daf9637a7dd6a9449f
Adrián Botías <abotias3@gmail.com>
2025-12-02 10:29:43 +0100

    Change image download source in ducky.sh

--- ducky.sh
+++ ducky.sh
@@ -3,8 +3,7 @@
 
 IMG="$HOME/Pictures/img.png"
 
-wget -q -O "$IMG" "https://cataas.com/cat/cute/says/hello"
-if [ $? -ne 0 ]; then wget -q -O "$IMG" "https://raw.githubusercontent.com/abotias/scripts/refs/heads/main/img/ducky.png"; fi
+wget -q -O "$IMG" "https://raw.githubusercontent.com/abotias/scripts/refs/heads/main/img/ducky.png"
 
 gsettings set org.gnome.desktop.background picture-uri "file://$IMG"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMG"
